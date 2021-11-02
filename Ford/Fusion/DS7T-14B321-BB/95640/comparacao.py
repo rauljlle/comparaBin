29a937608c188c73054179a905827b85b37db59d
Raul <rauljlle@gmail.com>
2021-11-02 03:37:55 -0300

    finalizacao monatagem array guia para comparacao

--- Ford/Fusion/DS7T-14B321-BB/95640/comparacao.py
+++ Ford/Fusion/DS7T-14B321-BB/95640/comparacao.py
@@ -1,18 +1,32 @@
 import difflib
+import os
 
-def clearCompara(nArquivos = 2, byts = bytearray()): #nArquivos= numero de arquivos clear na pasta
-    d = difflib.Differ()
-    x = open("clear"+str(nArquivos)+".bin", encoding = "ISO-8859-1").read()
-    y = open("clear"+str(nArquivos-1)+".bin", encoding = "ISO-8859-1").read()
-    e = d.compare(x,y)
-    print(list(e))    
-    # for i in range(0,len(list(e))):
-    #    if i.startswith("-"):
-    #        print(i + " <- index diferente")
+def comparaEndereco(listaDeListas, endereco):
+    arr = list()
+    for lista in listaDeListas:
+        arr.append(lista[endereco])
+    retorno = arr.count(arr[0]) == len(arr)
+    # if retorno == False:
+    #     print(arr)
+    return retorno
 
-    if nArquivos < 2:
-        clearCompara(nArquivos-2, byts)
+def montaArray(nArquivos = 2, byts = list()): #nArquivos= numero de arquivos clear na pasta
+    x = open(os.getcwd()+"/clear"+str(nArquivos)+".bin", "rb").read()
+    e = list()
+    for i in list(x):
+        e.append(hex(i))
+    
+    byts.append(e)
+    if nArquivos > 1:
+        retorno = montaArray(nArquivos-1, byts)
+        return retorno
+    else:
+        indexesFixos = list()
+        for i, hexa in enumerate(e):
+            if comparaEndereco(byts, i):
+                indexesFixos.append(i)
+        # print(indexesFixos)
+        return indexesFixos
 
-
-
-clearCompara()
+retorno = montaArray()
+print(retorno)
